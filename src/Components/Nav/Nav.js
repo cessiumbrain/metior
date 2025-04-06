26a1633725d86e20b53de70b4dbaa584e9cbf7f4
Joe Iannotta <jviannotta@gmail.com>
2025-04-06 01:07:13 -0400

    mocked up navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,8 +1,18 @@
 import './Nav.css'
+import { FaArrowDown } from 'react-icons/fa';
+import { useState } from 'react';
+
 function Nav(props){
+    const [navbarExpanded, setNavbarExpanded] = useState(false)
     return(
-        <div className="Nav">
-            <div onClick={()=>props.logout()} className="logout">LogOut</div>
+        <div className="Nav" onClick={()=>{setNavbarExpanded(!navbarExpanded)}}>
+            <FaArrowDown className="down-arrow"></FaArrowDown>
+            <div className={`nav-bar-dropdown ${navbarExpanded ? 'expanded' : 'collapsed'}`}>
+                <div onClick={()=>props.logout()} className="logout">LogOut</div>
+                <div className="home">Home</div>
+                <div className="profile">Profile</div>
+
+            </div>
         </div>
     )
 }
